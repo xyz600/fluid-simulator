6d2d1a07eba9aa2098f39ae7d54e4abdb1baedfb
xyz600 <xyz600600@gmail.com>
2022-05-24 01:11:49 +0900

    add some keybind

--- main.cu
+++ main.cu
@@ -17,6 +17,20 @@
 
 #include "cpu_fluid_simulator.hpp"
 
+struct ViewerState {
+    bool show_config;
+    ImVec4 clear_color;
+
+    int shape;
+
+    ViewerState()
+        : show_config(true)
+        , clear_color(ImVec4(0.45f, 0.55f, 0.60f, 1.00f))
+        , shape(0)
+    {
+    }
+};
+
 int main()
 {
     // GLFW + OpenGL
@@ -59,10 +73,7 @@ int main()
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
-    bool show_demo_window = true;
-    bool show_another_window = false;
+    ViewerState state;
 
     // CUDA with GL interop
     glGenBuffers(1, &pbo); // make & register PBO
@@ -74,9 +85,9 @@ int main()
 
     while (!glfwWindowShouldClose(window)) {
 
-        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo); // THE MAGIC LINE #1
+        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo);
         glDrawPixels(WIDTH, HEIGHT, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
-        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0); // THE MAGIC LINE #2
+        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
 
         simulator.draw_background();
 
@@ -89,40 +100,20 @@ int main()
 
         if (ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_Space))) {
             break;
+        } else if (ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_Tab))) {
+            state.show_config = !state.show_config;
         }
 
         // 設定周り色々
-        {
-            // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-            if (show_demo_window)
-                ImGui::ShowDemoWindow(&show_demo_window);
-
-            // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
-            {
-                static float f = 0.0f;
-                static int counter = 0;
-                ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
-                ImGui::Text("This is some useful text."); // Display some text (you can use a format strings too)
-                ImGui::Checkbox("Demo Window", &show_demo_window); // Edit bools storing our window open/close state
-                ImGui::Checkbox("Another Window", &show_another_window);
-                ImGui::SliderFloat("float", &f, 0.0f, 1.0f); // Edit 1 float using a slider from 0.0f to 1.0f
-                ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-                if (ImGui::Button("Button")) // Buttons return true when clicked (most widgets return true when edited/activated)
-                    counter++;
-                ImGui::SameLine();
-                ImGui::Text("counter = %d", counter);
-                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-                ImGui::End();
-            }
-
-            // 3. Show another simple window.
-            if (show_another_window) {
-                ImGui::Begin("Another Window", &show_another_window); // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-                ImGui::Text("Hello from another window!");
-                if (ImGui::Button("Close Me"))
-                    show_another_window = false;
-                ImGui::End();
-            }
+        if (state.show_config) {
+            ImGui::Begin("Config");
+            ImGui::RadioButton("square", &state.shape, 0);
+            ImGui::SameLine();
+            ImGui::RadioButton("Circle", &state.shape, 1);
+            ImGui::SameLine();
+            ImGui::RadioButton("None", &state.shape, 2);
+            
+            ImGui::End();
         }
 
         // Rendering
