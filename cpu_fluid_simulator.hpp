3b3f487d44de6788b91dfd14d538ea92a51f9342
xyz600 <xyz600600@gmail.com>
2022-05-24 16:28:52 +0900

    save

--- cpu_fluid_simulator.hpp
+++ cpu_fluid_simulator.hpp
@@ -5,11 +5,36 @@
 #include <cstdint>
 #include <memory>
 
-struct Color {
-    std::uint8_t x;
-    std::uint8_t y;
-    std::uint8_t z;
-    std::uint8_t w;
+#include "problem.hpp"
+
+using Color = Vec<std::uint8_t, 4>;
+
+template <typename T>
+class UpwindDifferenceCalculator {
+public:
+    UpwindDifferenceCalculator(const T* buffer, Vec2f* velocity, std::size_t height, std::size_t width)
+        : m_height_(height)
+        , m_width_(width)
+        , m_buffer_(buffer)
+        , m_velocity_(velocity)
+    {
+    }
+
+    std::pair<T, T> first_order_diff(const std::size_t y, const std::size_t x) const
+    {
+    }
+
+    std::pair<T, T> second_order_diff(const std::size_t y, const std::size_t x) const
+    {
+    }
+
+private:
+    Vec2f* m_velocity_;
+    T* m_buffer_;
+
+    std::size_t m_width_;
+
+    std::size_t m_height_;
 };
 
 class CPUFluidSimulator {
@@ -19,23 +44,36 @@ public:
         , height_(height)
         , pbo_(pbo)
         , buffer_(new Color[height * width])
-        , velocity_(new float[height * width])
-        , prev_velocity_(new float[height * width])
-        , pressure_(new float[height * width])
-        , prev_pressure_(new float[height * width])
+        , velocity_(new Vec2f[height * width])
+        , prev_velocity_(new Vec2f[height * width])
+        , pressure_(new Vec2f[height * width])
+        , prev_pressure_(new Vec2f[height * width])
+        , fixed_(new bool[height * width])
     {
+    }
+
+    void update()
+    {
+        // update v
+        velocity_.swap(prev_velocity_);
+
+        // update p
+        pressure_.swap(prev_pressure_);
     }
 
     void draw_background()
     {
         // 雑だけど、 glDrawPixels() を呼ぶところまでやる
+
+        update();
+
         for (std::size_t i = 0; i < height_; i++) {
             for (std::size_t j = 0; j < width_; j++) {
                 const std::uint8_t val = ((i + j) * 10) % 255;
-                buffer_[i * width_ + j].x = val;
-                buffer_[i * width_ + j].y = val;
-                buffer_[i * width_ + j].z = val;
-                buffer_[i * width_ + j].w = val;
+                buffer_[i * width_ + j].x() = val;
+                buffer_[i * width_ + j].y() = val;
+                buffer_[i * width_ + j].z() = val;
+                buffer_[i * width_ + j].w() = val;
             }
         }
         glDrawPixels(width_, height_, GL_RGBA, GL_UNSIGNED_BYTE, buffer_.get());
@@ -51,11 +89,13 @@ private:
     // color buffer
     std::unique_ptr<Color[]> buffer_;
 
-    std::unique_ptr<float[]> velocity_;
+    std::unique_ptr<Vec2f[]> velocity_;
 
-    std::unique_ptr<float[]> prev_velocity_;
+    std::unique_ptr<Vec2f[]> prev_velocity_;
 
-    std::unique_ptr<float[]> pressure_;
+    std::unique_ptr<Vec2f[]> pressure_;
 
-    std::unique_ptr<float[]> prev_pressure_;
+    std::unique_ptr<Vec2f[]> prev_pressure_;
+
+    std::unique_ptr<bool[]> fixed_;
 };
--- problem.hpp
+++ problem.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <array>
+#include <cassert>
 #include <cstdint>
+#include <iostream>
 
 struct Problem {
     std::size_t width;
@@ -8,11 +11,146 @@ struct Problem {
     std::size_t height;
 };
 
-struct Vec2 {
-    float x;
-    float y;
+template <typename T, std::size_t N>
+class Vec {
+public:
+    Vec()
+    {
+        m_data_.fill(0);
+    }
+
+    T operator[](const std::size_t index) const
+    {
+        return m_data_[index];
+    }
+
+    T& operator[](const std::size_t index)
+    {
+        return m_data_[index];
+    }
+
+    T x() const
+    {
+        assert(N >= 1);
+        return m_data_[0];
+    }
+
+    T& x()
+    {
+        assert(N >= 1);
+        return m_data_[0];
+    }
+
+    T y() const
+    {
+        assert(N >= 2);
+        return m_data_[1];
+    }
+
+    T& y()
+    {
+        assert(N >= 2);
+        return m_data_[1];
+    }
+
+    T z() const
+    {
+        assert(N >= 3);
+        return m_data_[2];
+    }
+
+    T& z()
+    {
+        assert(N >= 3);
+        return m_data_[2];
+    }
+
+    T w() const
+    {
+        assert(N >= 4);
+        return m_data_[3];
+    }
+
+    T& w()
+    {
+        assert(N >= 4);
+        return m_data_[3];
+    }
+
+    // 手抜き
+    auto begin()
+    {
+        return m_data_.begin();
+    }
+
+    auto end()
+    {
+        return m_data_.end();
+    }
+
+private:
+    std::array<T, N> m_data_;
 };
 
-struct State {
-    Vec2 velocity;
-};
+template <typename T>
+using Vec2 = Vec<T, 2>;
+
+using Vec2f = Vec<float, 2>;
+using Vec2d = Vec<double, 2>;
+
+template <typename T>
+using Vec3 = Vec<T, 3>;
+
+using Vec3f = Vec<float, 3>;
+using Vec3d = Vec<double, 3>;
+
+template <typename T, std::size_t N>
+std::ostream& operator<<(std::ostream& out, const Vec<T, N>& vec)
+{
+    out << "[";
+    for (auto v : vec) {
+        out << v << ", ";
+    }
+    out << "]";
+    return out;
+}
+
+template <typename T, std::size_t N>
+Vec<T, N> operator+(const Vec<T, N> v1, const Vec<T, N> v2)
+{
+    Vec<T, N> ret;
+    for (std::size_t i = 0; i < N; i++) {
+        ret[i] = v1[i] + v2[i];
+    }
+    return ret;
+}
+
+template <typename T, std::size_t N>
+Vec<T, N> operator-(const Vec<T, N> v1, const Vec<T, N> v2)
+{
+    Vec<T, N> ret;
+    for (std::size_t i = 0; i < N; i++) {
+        ret[i] = v1[i] - v2[i];
+    }
+    return ret;
+}
+
+template <typename T, std::size_t N>
+Vec<T, N> operator*(const Vec<T, N> v, const T coef)
+{
+    Vec<T, N> ret;
+    for (std::size_t i = 0; i < N; i++) {
+        ret[i] = v[i] * coef;
+    }
+    return ret;
+}
+
+template <typename T, std::size_t N>
+Vec<T, N> operator/(const Vec<T, N> v, const T coef)
+{
+    Vec<T, N> ret;
+    for (std::size_t i = 0; i < N; i++) {
+        ret[i] = v[i] / coef;
+    }
+    return ret;
+}
