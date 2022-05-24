71eb80f43f2e6faa3a2eebb1b2bd42e5b48072cc
xyz600 <xyz600600@gmail.com>
2022-05-24 17:48:45 +0900

    save

--- cpu_fluid_simulator.hpp
+++ cpu_fluid_simulator.hpp
@@ -7,12 +7,10 @@
 
 #include "problem.hpp"
 
-using Color = Vec<std::uint8_t, 4>;
-
 template <typename T>
 class UpwindDifferenceCalculator {
 public:
-    UpwindDifferenceCalculator(const T* buffer, Vec2f* velocity, std::size_t height, std::size_t width)
+    UpwindDifferenceCalculator(T* buffer, Vec2f* velocity, std::size_t height, std::size_t width)
         : m_height_(height)
         , m_width_(width)
         , m_buffer_(buffer)
@@ -36,8 +34,8 @@ public:
         assert(1 <= x && x < m_width_ - 1 && 1 <= y && y < m_height_ - 1);
 
         const auto index = y * m_width_ + x;
-        const auto x_val = m_buffer_[index + 1] + m_buffer_[index - 1] - 2 * m_buffer_[index];
-        const auto y_val = m_buffer_[index + m_width_] + m_buffer_[index - m_width_] - 2 * m_buffer_[index];
+        const auto x_val = m_buffer_[index + 1] + m_buffer_[index - 1] - m_buffer_[index] * 2.0f;
+        const auto y_val = m_buffer_[index + m_width_] + m_buffer_[index - m_width_] - m_buffer_[index] * 2.0f;
 
         return std::make_pair(x_val, y_val);
     }
@@ -52,69 +50,124 @@ private:
     std::size_t m_height_;
 };
 
+using Color = Vec<std::uint8_t, 4>;
+
 class CPUFluidSimulator {
 public:
-    CPUFluidSimulator(const std::size_t width, const std::size_t height, GLuint pbo)
-        : width_(width)
-        , height_(height)
-        , pbo_(pbo)
-        , buffer_(new Color[height * width])
-        , velocity_(new Vec2f[height * width])
-        , prev_velocity_(new Vec2f[height * width])
-        , pressure_(new Vec2f[height * width])
-        , prev_pressure_(new Vec2f[height * width])
-        , fixed_(new bool[height * width])
+    CPUFluidSimulator(const std::size_t width, const std::size_t height, GLuint pbo, float Re)
+        : m_width_(width)
+        , m_height_(height)
+        , m_pbo_(pbo)
+        , m_buffer_(new Color[height * width])
+        , m_velocity_(new Vec2f[height * width])
+        , m_prev_velocity_(new Vec2f[height * width])
+        , m_pressure_(new float[height * width])
+        , m_prev_pressure_(new float[height * width])
+        , m_fixed_(new bool[height * width])
+        , m_Re_(Re)
     {
+    }
+
+    void update_velocity()
+    {
+        const auto velocity_calculator = UpwindDifferenceCalculator<Vec2f>(m_prev_velocity_.get(), m_prev_velocity_.get(), m_height_, m_width_);
+
+        const auto pressure_calculator = UpwindDifferenceCalculator<float>(m_prev_pressure_.get(), m_prev_velocity_.get(), m_height_, m_width_);
+
+        // 境界条件の 1px 内側に関して、全て更新する
+        for (std::size_t y = 1; y < m_height_ - 1; y++) {
+            for (std::size_t x = 1; x < m_width_ - 1; x++) {
+                const auto index = y * m_width_ + x;
+
+                const auto [dx1, dy1] = velocity_calculator.first_order_diff(y, x);
+                const auto term1 = -(dx1 * m_prev_velocity_[index].x() + dy1 * m_prev_velocity_[index].y());
+
+                const auto [dx2, dy2] = pressure_calculator.first_order_diff(y, x);
+                const auto term2 = -Vec2f(dx2, dy2);
+
+                const auto [dx3, dy3] = velocity_calculator.second_order_diff(y, x);
+                const auto term3 = (dx3 + dy3) / m_Re_;
+
+                m_velocity_[index] = term1 + term2 + term3;
+            }
+        }
+    }
+
+    void update_pressure()
+    {
+        const auto velocity_calculator = UpwindDifferenceCalculator<Vec2f>(m_prev_velocity_.get(), m_prev_velocity_.get(), m_height_, m_width_);
+
+        constexpr std::size_t MaxIter = 5;
+
+        for (std::size_t iter = 0; iter < MaxIter; iter++) {
+            // 境界条件の 1px 内側に関して、全て更新する
+            for (std::size_t y = 1; y < m_height_ - 1; y++) {
+                for (std::size_t x = 1; x < m_width_ - 1; x++) {
+                    const auto index = y * m_width_ + x;
+
+                    const auto [dx, dy] = velocity_calculator.first_order_diff(y, x);
+                }
+            }
+
+            if (iter < MaxIter - 1) {
+                // 反復法のために swap
+                m_pressure_.swap(m_prev_pressure_);
+            }
+        }
+        // m_pressure_ に答えが入っている
     }
 
     void update()
     {
-        // update v
-        velocity_.swap(prev_velocity_);
+        m_velocity_.swap(m_prev_velocity_);
+        m_pressure_.swap(m_prev_pressure_);
 
-        // update p
-        pressure_.swap(prev_pressure_);
+        update_velocity();
+
+        update_pressure();
     }
 
     void draw_background()
     {
         // 雑だけど、 glDrawPixels() を呼ぶところまでやる
-
         update();
 
-        for (std::size_t i = 0; i < height_; i++) {
-            for (std::size_t j = 0; j < width_; j++) {
-                const std::uint8_t val = ((i + j) * 10) % 255;
-                buffer_[i * width_ + j].x() = val;
-                buffer_[i * width_ + j].y() = val;
-                buffer_[i * width_ + j].z() = val;
-                buffer_[i * width_ + j].w() = val;
+        for (std::size_t i = 0; i < m_height_; i++) {
+            for (std::size_t j = 0; j < m_width_; j++) {
+                const auto index = i * m_width_ + j;
+                m_buffer_[index].x() = (i + j) % 255;
+                m_buffer_[index].y() = (i * j) % 255;
+                m_buffer_[index].z() = (i * 25500000 - j) % 255;
+                m_buffer_[index].w() = 255;
             }
         }
-        glDrawPixels(width_, height_, GL_RGBA, GL_UNSIGNED_BYTE, buffer_.get());
+        glDrawPixels(m_width_, m_height_, GL_RGBA, GL_UNSIGNED_BYTE, m_buffer_.get());
     }
 
 private:
-    std::size_t width_;
+    std::size_t m_width_;
 
-    std::size_t height_;
+    std::size_t m_height_;
 
-    GLuint pbo_;
+    GLuint m_pbo_;
 
     // color buffer
-    std::unique_ptr<Color[]> buffer_;
+    std::unique_ptr<Color[]> m_buffer_;
 
-    std::unique_ptr<Vec2f[]> velocity_;
+    std::unique_ptr<Vec2f[]> m_velocity_;
 
-    std::unique_ptr<Vec2f[]> prev_velocity_;
+    std::unique_ptr<Vec2f[]> m_prev_velocity_;
 
-    std::unique_ptr<Vec2f[]> pressure_;
+    std::unique_ptr<float[]> m_pressure_;
 
-    std::unique_ptr<Vec2f[]> prev_pressure_;
+    std::unique_ptr<float[]> m_prev_pressure_;
 
-    std::unique_ptr<Color[]> color_;
+    std::unique_ptr<Color[]> m_color_;
 
-    std::unique_ptr<Color[]> prev_color_;
+    std::unique_ptr<Color[]> m_prev_color_;
 
-    std::unique_ptr<bool[]> fixed_;
+    std::unique_ptr<bool[]> m_fixed_;
+
+    // レイノルズ数
+    float m_Re_;
 };
--- problem.hpp
+++ problem.hpp
@@ -14,9 +14,30 @@ struct Problem {
 template <typename T, std::size_t N>
 class Vec {
 public:
+    using value_t = T;
+
     Vec()
     {
         m_data_.fill(0);
+    }
+
+private:
+    template <class... Args>
+    void inner_assign(const std::size_t index)
+    {
+    }
+
+    template <class... Args>
+    void inner_assign(const std::size_t index, T val, Args... args)
+    {
+        m_data_[index] = val;
+        inner_assign(index + 1, args...);
+    }
+
+public:
+    template <class... Args>
+    Vec(Args... args)
+    {
     }
 
     T operator[](const std::size_t index) const
@@ -151,6 +172,26 @@ Vec<T, N> operator/(const Vec<T, N> v, const T coef)
     Vec<T, N> ret;
     for (std::size_t i = 0; i < N; i++) {
         ret[i] = v[i] / coef;
+    }
+    return ret;
+}
+
+template <typename T, std::size_t N>
+Vec<T, N> operator-(const Vec<T, N> v)
+{
+    Vec<T, N> ret;
+    for (std::size_t i = 0; i < N; i++) {
+        ret[i] = -v[i];
+    }
+    return ret;
+}
+
+template <typename T, std::size_t N>
+T dot(const Vec<T, N>& v1, const Vec<T, N>& v2)
+{
+    T ret = 0;
+    for (std::size_t i = 0; i < N; i++) {
+        ret += v1[i] * v2[i];
     }
     return ret;
 }
